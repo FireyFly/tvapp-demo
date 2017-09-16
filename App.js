86408c9c7194a519791e6f5d3538fc1ffc1fc43d
Jonas Höglund <firefly@firefly.nu>
2017-09-16 15:48:46 +0200

    Refactor: move SearchField and ResultsView to own files

--- App.js
+++ App.js
@@ -1,92 +1,10 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View, Image, ActivityIndicator,
-         SectionList, TextInput } from 'react-native';
-
-//-- SearchField ----------------------------------------------------
-function debounce(fn) {
-  let timeout = null
-  return function (delay, ...args) {
-    // Expire any previous timeout
-    if (timeout != null) clearTimeout(timeout)
-    // Call immediately or with a delay
-    if (delay == 0) fn.apply(this, args)
-    else timeout = setTimeout(() => fn.apply(this, args), delay)
-  }
-}
-
-class SearchField extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {text: ''}
-    this.debouncedOnSearch = debounce(query => this.props.onSearch(query))
-  }
-  render() {
-    return <TextInput {...this.props}
-             style={styles.searchField}
-             onChangeText={ str => {
-               this.setState({text: str})
-               this.props.onPreSearch()
-               this.debouncedOnSearch(600, str)
-             } }
-             onEndEditing={ () => this.debouncedOnSearch(0, this.state.text) }
-             editable={true}
-             placeholder="Search…"
-             returnKeyType="search"
-             value={this.state.text} />
-  }
-}
-
-//-- ResultsView ----------------------------------------------------
-class ResultsView extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      isLoading: false,
-      title:     "",
-      sections:  [],
-    }
-    if (props.onConstruct != null) props.onConstruct(this)
-  }
-
-  render() {
-    if (this.state.isLoading) {
-      return <View style={{flex:1, alignItems:'center', justifyContent:'center' }}>
-               <ActivityIndicator size="large" />
-             </View>
-    }
-
-    let formatEpisode = num => 'E' + (num < 10? '0' : '') + num
-    let renderItem = ({item}) =>
-        <View style={[resultsStyles.itemContainer, {marginLeft:10, marginRight:10}]}>
-          <Image style={resultsStyles.itemImage} source={{uri: item.image}} />
-          <Text style={[resultsStyles.itemText, {marginLeft:10, width:48}]}>{formatEpisode(item.episode)}</Text>
-          <Text style={resultsStyles.itemText}>{item.name}</Text>
-        </View>
-
-    let renderSectionHeader = ({section}) =>
-        <View style={[resultsStyles.itemContainer, {backgroundColor:'#999'}]}>
-          <Text style={[resultsStyles.itemText, {marginLeft:10, color:'#EEE'}]}>{section.title}</Text>
-        </View>
-
-    return <View style={{flex:1}}>
-             <Text style={resultsStyles.title}>{this.state.title}</Text>
-             <SectionList
-                renderItem={renderItem}
-                renderSectionHeader={renderSectionHeader}
-                sections={this.state.sections} />
-           </View>
-  }
-}
-const resultsStyles = StyleSheet.create({
-  itemContainer: { flex:1, flexDirection:'row', padding:2, height:44, alignItems:'center' },
-  itemImage: { width: 40, height: 40, borderRadius: 4 },
-  itemText: { fontSize:18 },
-  title: { fontSize:28 },
-});
+import React, {Component} from 'react'
+import {StyleSheet, View} from 'react-native'
+import SearchField from './lib/SearchField.js'
+import ResultsView from './lib/ResultsView.js'
 
 //-- performSearch --------------------------------------------------
 const noPictureURI = 'http://firefly.nu/up/me.jpg'
-
 function performSearch(query, resultsComponent) {
   // Skip API request if the search query is empty
   if (query.trim().length == 0) {
@@ -129,7 +47,7 @@ export default class App extends Component {
   render() {
     let resultsView = null
     return <View style={styles.container}>
-             <SearchField
+             <SearchField style={{margin: 10}}
                onPreSearch={ () => resultsView.setState({ isLoading: true }) }
                onSearch={ query => performSearch(query, resultsView) } />
              <ResultsView onConstruct={ obj => {resultsView = obj} } />
@@ -137,18 +55,10 @@ export default class App extends Component {
   }
 }
 
-//-- Stylesheet -----------------------------------------------------
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 22,
     backgroundColor: '#EEE',
   },
-
-  searchField: {
-    backgroundColor: '#D3D3D3',
-    margin: 10,
-    padding: 6,
-    borderRadius: 8,
-  },
-});
+})
